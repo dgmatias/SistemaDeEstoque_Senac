58ea2363a9efbe14ea7fc8e118b50e6c1c2b08af
Frederico Gondin <108304623+fgondin@users.noreply.github.com>
2022-11-19 21:11:35 -0300

    Add files via upload

--- sistema-estoque/index.php
+++ sistema-estoque/index.php
@@ -1,3 +1,27 @@
+<?php
+
+session_start();
+ob_start();
+
+if((!isset($_SESSSION['id'])) && (!isset($_SESSION['nome']))){
+    $_SESSION['msg'] = "<p style='color: #ff0000'>Erro: Necessário fazer o login!</p>";
+    header("Location: index.php");
+}
+
+require 'config.php';
+
+$lista = [];
+
+$sql = $pdo->query("SELECT * FROM produtos");
+
+if($sql->rowCount() > 0){
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
+//Lista input get
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +32,57 @@
 </head>
 <body>
 
-    <h1>Parabéns!</h1>
+    <div class="container">
+
+        <h2> Pesquisar </h2>
+
+        <form action="" method="get">
+
+            <input type="search" name="item1" id="" placeholder="Buscar por item 1">
+            <input type="search" name="item2" id="" placeholder="Buscar por item 2">
+
+        </form>
+
+    </div>
+
+    <div>
+
+        <table class="table">
+
+            <tr>
+
+                <th> Item 1 </th>
+                <th> Item 2 </th>
+
+            </tr>
+
+            <?php foreach($lista as $usuario): ?>
+                <tr>
+
+                    <td> <?php echo $usuario['item1'];?> </td>
+                    <td> <?php $usuario ['item2']; ?> </td>
+                    <td>
+
+                        <a href="editar.php?id=<?=$usuario['id']; ?>"
+                            class="btn brn-success"
+                        > Editar </a>
+
+                        <a href="excluir.php?id=<?$usuario['id']; ?>"
+                        onclick="return confirm('Tem certeza que deseja exluir?')"
+                        class="btn btn-danger"
+                        > Excluir </a>
+
+                    </td>
+
+                </tr>
+            <?php endforeach; ?>   
+        </table>
+
+        <a href="adicionar.php" class="btn btn-primary"> Adicionar </a>
+
+        <a href="./logout.php">Sair</a>
+
+    </div>
     
 </body>
 </html>
