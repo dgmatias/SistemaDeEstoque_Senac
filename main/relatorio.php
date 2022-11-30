f4c21aae407ade7d30a1a32ac6d86cfa8c8ae6c5
Thiago <47253697+ThiagoP08@users.noreply.github.com>
2022-11-30 20:40:08 -0300

    Add files via upload

---
+++ main/relatorio.php
@@ -0,0 +1,65 @@
+<?php
+
+    require 'config.php';
+
+    session_start();
+    ob_start();
+
+
+    $lista = [];
+
+    $sql = $pdo->query("SELECT * FROM produtos WHERE quantidade < min_quantidade;");
+
+    if($sql->rowCount() > 0){
+
+        $lista = $sql->fetchall(PDO::FETCH_ASSOC);
+
+    };      
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Relatório </title>
+</head>
+<body>
+    <div class="container">
+        
+        <h1>Relatório</h1>
+
+        <div class="imprimir">
+            <button onclick="imprimir()"> imprimir </button>
+        </div>
+    
+        <table class="table">
+    
+            <tr>
+                <th> Nome </th>
+                <th> Quantidade </th>
+                <th> Quantidade mínima </th>
+    
+            </tr>
+    
+            <?php foreach($lista as $usuario): ?>
+                
+                <tr>
+                    <td> <?php echo $usuario['nome']?> </td>
+                    <td> <?php echo $usuario['quantidade']?> </td>
+                    <td> <?php echo $usuario['min_quantidade']?> </td>
+                </tr>
+    
+            <?php endforeach; ?>   
+        </table>
+    
+        <script type="text/javascript"> function imprimir(){
+            window.print();
+        } </script>
+        
+    </div>
+</body>
+</html>
+
