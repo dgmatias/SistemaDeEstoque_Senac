4091cde4da27034345db84b0c297f22167f838ac
Diego Matias <dg.matias021@gmail.com>
2022-11-30 21:41:45 -0300

    Add files via upload

---
+++ main/header.php
@@ -0,0 +1,32 @@
+<div id="menu-container">
+
+            <div>
+
+                <div class="item-menu"> <img src="" alt="logotipo">  </div>
+                
+                <div class="item-menu"> <span> DTFJ Depositos </span> </div>
+                
+            </div>
+
+
+            <div>
+
+                <div class="item-menu"> <a href=""> Home </a> </div>
+
+                <div class="item-menu"> <a href="adicionar.php"> Adicionar produtos </a> </div>
+
+                <div class="item-menu"> <a href="relatorio.php"> Relatório </a> </div>
+
+            </div>  
+
+            <div> 
+                
+                <div class="item-menu"> <img src="" alt="avatar"> </div>
+            
+                <div class="item-menu"> <span> Nome </span> </div>
+
+                <div class="item-menu"> <a href="logout.php"> sair </a> </div>
+
+            </div>
+
+        </div>
